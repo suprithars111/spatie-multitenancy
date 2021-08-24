3ff656ef039e64f3575c0c24e472ae93dbfef0d1
Supritha R S <suprithars111@gmail.com>
2021-08-24 18:47:51 +0530

    Feat : Multiple database added

--- config/multitenancy.php
+++ config/multitenancy.php
@@ -18,7 +18,7 @@ return [
      * This class should extend `Spatie\Multitenancy\TenantFinder\TenantFinder`
      *
      */
-    'tenant_finder' => null,
+    'tenant_finder' => Spatie\Multitenancy\TenantFinder\DomainTenantFinder::class,
 
     /*
      * These fields are used by tenant:artisan command to match one or more tenant
@@ -33,7 +33,8 @@ return [
      * A valid task is any class that implements Spatie\Multitenancy\Tasks\SwitchTenantTask
      */
     'switch_tenant_tasks' => [
-        // add tasks here
+        Spatie\Multitenancy\Tasks\SwitchTenantDatabaseTask::class,
+
     ],
 
     /*
@@ -60,7 +61,7 @@ return [
     /*
      * The connection name to reach the landlord database
      */
-    'landlord_database_connection_name' => null,
+    'landlord_database_connection_name' => 'landlord',
 
     /*
      * This key will be used to bind the current tenant in the container.
